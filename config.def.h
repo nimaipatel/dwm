4da791c8970f279e1e98981249eaa9bad8d97668
nimai <nimai.m.patel@gmail.com>
2021-04-09 16:08:22 +0530

    saner bindings

--- config.def.h
+++ config.def.h
@@ -1,5 +1,4 @@
-/* See LICENSE file for copyright and license details. */
-#include <X11/XF86keysym.h>
+/* See LICENSE file for copyright and license details. */ #include <X11/XF86keysym.h>
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
@@ -45,7 +44,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -66,13 +65,13 @@ static Key keys[] = {
 	{ MODKEY,                       XK_b,                              togglebar,             {0} },
 	{ MODKEY,                       XK_j,                              focusstack,            {.i = +1 } },
 	{ MODKEY,                       XK_k,                              focusstack,            {.i = -1 } },
-	{ MODKEY,                       XK_i,                              incnmaster,            {.i = +1 } },
-	{ MODKEY,                       XK_d,                              incnmaster,            {.i = -1 } },
+	{ MODKEY,                       XK_o,                              incnmaster,            {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_o,                              incnmaster,            {.i = -1 } },
 	{ MODKEY,                       XK_h,                              setmfact,              {.f = -0.05} },
 	{ MODKEY,                       XK_l,                              setmfact,              {.f = +0.05} },
-	{ MODKEY,                       XK_Return,                         zoom,                  {0} },
+	{ MODKEY|ShiftMask,             XK_Return,                         zoom,                  {0} },
 	{ MODKEY,                       XK_Tab,                            view,                  {0} },
-	{ MODKEY|ShiftMask,             XK_c,                              killclient,            {0} },
+	{ MODKEY|ShiftMask,             XK_q,                              killclient,            {0} },
 	{ MODKEY,                       XK_t,                              setlayout,             {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,                              setlayout,             {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,                              setlayout,             {.v = &layouts[2]} },
@@ -117,7 +116,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_z,      quit,           {0} },
 };
 
 /* button definitions */
