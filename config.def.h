7292e581cf90f72252790fbe5dcd4fd86696e1b1
nimai <nimai.m.patel@gmail.com>
2021-04-09 16:13:42 +0530

    gaps

--- config.def.h
+++ config.def.h
@@ -2,6 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 0;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -65,6 +66,9 @@ static Key keys[] = {
 	/* modifier                     key                                function               argument */
 	{ MODKEY,                       XK_Return,                         spawn,                 {.v = termcmd } },
 	{ MODKEY,                       XK_b,                              togglebar,             {0} },
+	{ MODKEY,                       XK_minus,                          setgaps,               {.i = -1 } },
+	{ MODKEY,                       XK_equal,                          setgaps,               {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_equal,                          setgaps,               {.i = 0  } },
 	{ MODKEY,                       XK_j,                              focusstack,            {.i = +1 } },
 	{ MODKEY,                       XK_k,                              focusstack,            {.i = -1 } },
 	{ MODKEY,                       XK_o,                              incnmaster,            {.i = +1 } },
