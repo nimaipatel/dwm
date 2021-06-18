3993cb58c17a250d1bed40ef7b1dd4f9b0f7ee6f
nimai <nimai.m.patel@gmail.com>
2021-06-18 17:51:54 +0530

    Make mpv, et cetera fill properly instead of weirdly keeping it empty

--- config.def.h
+++ config.def.h
@@ -57,7 +57,7 @@ static const char swalsymbol[] = "👅";
 /* layout(s) */
 static float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static int nmaster     = 1;    /* number of clients in master area */
-static int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
