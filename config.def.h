e5874f2f0b7bcea50017ba652472393c7948940c
nimai <nimai.m.patel@gmail.com>
2021-06-18 21:17:56 +0530

    Don't use bool, use int

--- config.def.h
+++ config.def.h
@@ -8,7 +8,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 20;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const int focusonwheel       = 0;
-static const Bool viewontag         = True;     /* Switch view on tag switch */
+static const int viewontag         = 1;     /* Switch view on tag switch */
 static char font[]            = "monospace:size=8";
 static char dmenufont[]       = "monospace:size=8";
 static const char *fonts[]          = { font, "joypixels:size=8", "Symbols Nerd Font:size=8",  };
