b2f8f2cbc3b09f8e897fffade4026e60ab61456d
nimai <nimai.m.patel@gmail.com>
2021-07-27 23:22:22 +0530

    add fzfmenu support to dwm

--- config.def.h
+++ config.def.h
@@ -50,10 +50,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance      title    tags mask     isfloating   monitor */
-	{ "Gimp",       NULL,         NULL,      0,            1,           -1 },
-	{ NULL,         "ncmpcpp",    NULL,      SPTAG(0),     1,           -1 },
-	{ NULL,         "scratch",    NULL,      SPTAG(1),     1,           -1 },
+	/* class      instance      title          tags mask     isfloating   monitor */
+	{ "Gimp",       NULL,         NULL,        0,            1,           -1 },
+	{ "fzfmenu",    NULL,         "fzfmenu",   0,            1,           -1 },
+	{ NULL,         "ncmpcpp",    NULL,        SPTAG(0),     1,           -1 },
+	{ NULL,         "scratch",    NULL,        SPTAG(1),     1,           -1 },
 };
 
 /* window swallowing */
