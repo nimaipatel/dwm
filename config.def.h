e4f10e0f79aa1c2faca84c6adc26df7ade079867
nimai <nimai.m.patel@gmail.com>
2022-01-10 21:18:23 +0530

    Status2d with xrdb support

--- config.def.h
+++ config.def.h
@@ -6,18 +6,52 @@ static const unsigned int gappx     = 0;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const int user_bh            = 20;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const int user_bh            = 30;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const int focusonwheel       = 0;
 static const int viewontag         = 1;     /* Switch view on tag switch */
 static char font[]            = "monospace:size=12";
 static char dmenufont[]       = "monospace:size=8";
-static const char *fonts[]          = { font, "joypixels:size=10", "Symbols Nerd Font:size=10",  };
+static const char *fonts[]          = { font, "joypixels:size=10", "Symbols Nerd Font:size=20",  };
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
 static char selfgcolor[]            = "#eeeeee";
 static char selbordercolor[]        = "#005577";
 static char selbgcolor[]            = "#005577";
+static char termcol0[] = "#000000"; /* black   */
+static char termcol1[] = "#ff0000"; /* red     */
+static char termcol2[] = "#33ff00"; /* green   */
+static char termcol3[] = "#ff0099"; /* yellow  */
+static char termcol4[] = "#0066ff"; /* blue    */
+static char termcol5[] = "#cc00ff"; /* magenta */
+static char termcol6[] = "#00ffff"; /* cyan    */
+static char termcol7[] = "#d0d0d0"; /* white   */
+static char termcol8[]  = "#808080"; /* black   */
+static char termcol9[]  = "#ff0000"; /* red     */
+static char termcol10[] = "#33ff00"; /* green   */
+static char termcol11[] = "#ff0099"; /* yellow  */
+static char termcol12[] = "#0066ff"; /* blue    */
+static char termcol13[] = "#cc00ff"; /* magenta */
+static char termcol14[] = "#00ffff"; /* cyan    */
+static char termcol15[] = "#ffffff"; /* white   */
+static char *termcolor[] = {
+	termcol0,
+	termcol1,
+	termcol2,
+	termcol3,
+	termcol4,
+	termcol5,
+	termcol6,
+	termcol7,
+	termcol8,
+	termcol9,
+	termcol10,
+	termcol11,
+	termcol12,
+	termcol13,
+	termcol14,
+	termcol15,
+};
 static char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
@@ -103,6 +137,22 @@ ResourcePref resources[] = {
 		{ "nmaster",          	INTEGER, &nmaster },
 		{ "resizehints",       	INTEGER, &resizehints },
 		{ "mfact",      	 	FLOAT,   &mfact },
+    { "color0", STRING, &termcol0 },
+    { "color1", STRING, &termcol1 },
+    { "color2", STRING, &termcol2 },
+    { "color3", STRING, &termcol3 },
+    { "color4", STRING, &termcol4 },
+    { "color5", STRING, &termcol5 },
+    { "color6", STRING, &termcol6 },
+    { "color7", STRING, &termcol7 },
+    { "color8", STRING, &termcol8 },
+    { "color9", STRING, &termcol9 },
+    { "color10", STRING, &termcol10 },
+    { "color11", STRING, &termcol11 },
+    { "color12", STRING, &termcol12 },
+    { "color13", STRING, &termcol13 },
+    { "color14", STRING, &termcol14 },
+    { "color15", STRING, &termcol15 },
 };
 
 static Key keys[] = {
