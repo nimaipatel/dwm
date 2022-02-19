02fa9481043f2e0a05cc5f56c5ebe3a6cb8e7474
nimai <nimai.m.patel@gmail.com>
2022-02-19 15:56:49 +0530

    setgaps sets to default for argument = 0 instead of setting it to 0

--- dwm.c
+++ dwm.c
@@ -1776,7 +1776,9 @@ setfullscreen(Client *c, int fullscreen)
 void
 setgaps(const Arg *arg)
 {
-	if ((arg->i == 0) || (selmon->gappx + arg->i < 0))
+	if (arg->i == 0)
+		selmon->gappx = gappx;
+	else if (selmon->gappx + arg->i < 0)
 		selmon->gappx = 0;
 	else
 		selmon->gappx += arg->i;
