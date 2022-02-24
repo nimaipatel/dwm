3d70a021af267844222c6cdbc9ad27d098a0ae80
nimai <nimai.m.patel@gmail.com>
2022-02-24 20:26:23 +0530

    Change license to GPLv3

--- config.def.h
+++ config.def.h
@@ -2,8 +2,8 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 10;        /* gaps between windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 0;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
@@ -230,7 +230,7 @@ static Key keys[] = {
 	/* search for unicode characters */
 	{ MODKEY,                       XK_u,                              spawn,                 SHCMD("unicode-selector") },
 
-	{ MODKEY,                       XK_e,                              spawn,                 SHCMD("$TERMINAL -e tmux new-session -As def") },
+	{ MODKEY,                       XK_e,                              spawn,                 SHCMD("(pidof emacs || (notify-send 'Starting Emacs daemon 🖊️' ;  emacs --daemon)) && emacsclient -c -n -e '(switch-to-buffer nil)'") },
 
 	/* search some websites for a term */
 	{ MODKEY,                       XK_s,                              spawn,                 SHCMD("browser-search") },
