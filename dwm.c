b13a11f34814cfa544ea521a3aae25e4a941ac52
nimai <nimai.m.patel@gmail.com>
2022-08-01 21:58:17 +0530

    clean up

--- dwm.c
+++ dwm.c
@@ -232,7 +232,6 @@ static int gettextprop(Window w, Atom atom, char *text, unsigned int size);
 static void grabbuttons(Client *c, int focused);
 static void grabkeys(void);
 static void incnmaster(const Arg *arg);
-static void inplacerotate(const Arg *arg);
 static void keypress(XEvent *e);
 static void killclient(const Arg *arg);
 static void manage(Window w, XWindowAttributes *wa);
